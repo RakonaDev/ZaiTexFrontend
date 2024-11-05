0e69eb1521c2fe3515f10349b857f0d9376425b5
RakonaDev <juancajas1905@gmail.com>
2024-11-04 23:01:07 -0500

    Fixing

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Event } from '@angular/router';
 
 @Component({
   selector: 'app-form',
@@ -7,7 +8,13 @@ import { Component } from '@angular/core';
 })
 export class FormComponent {
   producto: string = 'Qué producto te interesa?';
-  curso: string = this.producto === 'curso' ? 'Que curso te gustaría estudiar?' : 'Que taller te gustaría estudiar?';
+  curso: string = 'Que taller te gustaría estudiar?';
+  provincia: string = 'Escoja su Departamento de procedencia';
+  nombre: string = '';
+  apellido: string = '';
+  celular?: number;
+  dni: string = '';
+  email: string = '';
 
   cursos = [
     { opcion: 'Que curso te gustaría estudiar?', valor: 'Que curso te gustaría estudiar?' },
@@ -23,7 +30,46 @@ export class FormComponent {
     { opcion: 'Ilustración Digital', valor: 'Ilustración Digital' },
   ]
 
-  cambiarColor(event: any) {
-    event.target.style.color = 'black';
+  departamentosPeru = [
+    { opcion: 'Escoja su Departamento de procedencia', valor: 'Escoja su Departamento de procedencia'},
+    { opcion: "Lima", valor: "Lima" },
+    { opcion: "Arequipa", valor: "Arequipa" },
+    { opcion: "Cuzco", valor: "Cuzco" },
+    { opcion: "Piura", valor: "Piura" },
+    { opcion: "La Libertad", valor: "La Libertad" },
+    { opcion: "Lambayeque", valor: "Lambayeque" },
+    { opcion: "Puno", valor: "Puno" },
+    { opcion: "Junín", valor: "Junín" },
+    { opcion: "Amazonas", valor: "Amazonas" },
+    { opcion: "Ayacucho", valor: "Ayacucho" },
+    { opcion: "Apurímac", valor: "Apurímac" },
+    { opcion: "Huancavelica", valor: "Huancavelica" },
+    { opcion: "Pasco", valor: "Pasco" },
+    { opcion: "Huánuco", valor: "Huánuco" },
+    { opcion: "Ica", valor: "Ica" },
+    { opcion: "San Martín", valor: "San Martín" },
+    { opcion: "Ucayali", valor: "Ucayali" },
+    { opcion: "Tumbes", valor: "Tumbes" },
+    { opcion: "Madre de Dios", valor: "Madre de Dios" },
+    { opcion: "Callao", valor: "Callao" },
+    { opcion: "Ancash", valor: "Ancash" },
+    { opcion: "Loreto", valor: "Loreto" },
+    { opcion: "Tacna", valor: "Tacna" },
+    { opcion: "Moquegua", valor: "Moquegua" },
+    { opcion: "Cajamarca", valor: "Cajamarca" },
+    { opcion: "La Libertad", valor: "La Libertad" },
+    { opcion: "Lima Provincias", valor: "Lima Provincias" }
+];
+
+  cambiarColor(event: any, tipo: string = 'producto') {
+    if (tipo === 'producto') {
+      event.target.style.color = 'black';
+    }
+    if (event.target.value == 'curso') {
+      this.curso = 'Que curso te gustaría estudiar?'
+    }
+    else if (event.target.value == 'taller') {
+      this.curso = 'Que taller te gustaría estudiar?'
+    }
   }
 }
